2abcc1857cdfb749e6b2fc5a1774202154d73c31
AnhPhan <anhphan21@outlook.com>
2023-10-05 02:00:00 +0800

    v0.1.2: done parser

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include "./include/Graph.h"
 using namespace std;
 
-
-
 int main(int argc, char* argv[]) {
 
 	int AND_MAX;
@@ -14,11 +12,14 @@ int main(int argc, char* argv[]) {
 
 	string module_name;
 	vector<string> in_list, out_list;
+	vector<string> funct;
 
 	// Read blif file and extract data
 	ifstream 	blif_file(argv[1]);
 	int 		mode = 0;						//-2: read comment, -1: read module name, 0: read normal, 1: parse input, 2: parse output, 3: parse function
-	string 		temp;
+	int			func_flag = 0;					//0: parse variables, 1: parse operation
+	string 		temp, temp_func;
+	vector<string> vari_list;
 
 	
 	while(1) {
@@ -32,6 +33,7 @@ int main(int argc, char* argv[]) {
 				blif_file >> module_name;
 			else
 				module_name = argv[1];
+			continue;
 		}
 		else if (temp == ".inputs") {
 			mode = 1;
@@ -43,10 +45,24 @@ int main(int argc, char* argv[]) {
 		}
 		else if (temp == ".names") {
 			mode = 3;
+			if(func_flag == 1) {
+				temp_func.pop_back();
+				temp_func = temp_func + "="	+ vari_list[vari_list.size()-1];
+				funct.push_back(temp_func);
+			}
+			func_flag = 0;
+			vari_list.clear();
+			temp_func.clear();
 			continue;
 		}
-		else if(temp == ".end") break;
-
+		else if(temp == ".end") {
+			if(func_flag == 1) {
+				temp_func.pop_back();
+				temp_func = temp_func + "="	+ vari_list[vari_list.size()-1];
+				funct.push_back(temp_func);
+			}
+			break;
+		}
 		if (mode == 1) {
 			if (temp == "\\")
 				continue;
@@ -60,14 +76,33 @@ int main(int argc, char* argv[]) {
 				out_list.push_back(temp);
 		}
 		else if (mode == 3) {
-			
+			if (func_flag == 0) {													//1st time, code read the variables, if meet the \ continure, if meet 
+				if (temp == "\\") {
+					continue;
+				} else {
+					vari_list.push_back(temp);
+					if ((blif_file.peek() == '\n'))
+						func_flag = 1;
+				}
+			}
+			else if (func_flag == 1) {
+				for (int i = 0; i < temp.size(); i++) {
+					if (temp[i] == '0')
+						temp_func = temp_func + vari_list[i] +'\'';
+					else if (temp[i] == '1')
+						temp_func = temp_func + vari_list[i];
+				}
+				temp_func += "+";
+				blif_file >> temp;
+			}
 		}
 	}
 
 	blif_file.close();
-	cout << "Done read file" << endl;
+	cout << "Done reading file" << endl;
 
 	// Print file result
+	cout << "-----------------------------" << endl;
 	cout << module_name << endl;
 	cout << "Inputs: " ;
 	for (int i = 0; i < in_list.size(); i++)
@@ -77,6 +112,11 @@ int main(int argc, char* argv[]) {
 	for (int i = 0; i < out_list.size(); i++)
 		cout << out_list[i] << " ";
 	cout << endl;
+	for (int i = 0; i < funct.size(); i++)
+		cout << funct[i] << endl;
 	cout << "-----------------------------" << endl;
 	return 0;
 }
+
+// Changelog
+// Still error when have the comment at the end of line
