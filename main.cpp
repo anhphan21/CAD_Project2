75c9691e837d89889df74112d3e2e5edfa7978f6
AnhPhan <anhphan21@outlook.com>
2023-10-04 13:30:14 +0800

    v0.1 continure

--- main.cpp
+++ main.cpp
@@ -15,34 +15,65 @@ int main(int argc, char* argv[]) {
 	string module_name;
 
 	// Read blif file and extract data
-	ifstream blif_file(argv[1]);
-	string temp;
+	ifstream 	blif_file(argv[1]);
+	int 		mode = 0;						//-2: read comment, -1: read module name, 0: read normal, 1: parse input, 2: parse output, 3: parse function
+	string 		temp;
+
 	while(1) {
 		blif_file >> temp;
 		if (temp[0] == '#') {					//check if we get the comment part
+			mode = -2;
+		}
+		else if (temp == ".model")	{			//get module name
+			// if (blif_file.peek() != '\n')
+			// 	blif_file >> module_name;
+			// else
+			// 	module_name = argv[1];
+			mode = -1;
+		}
+		else if (temp == ".inputs") {
+			mode = 1;
+		}
+		else if (temp == ".outputs") {
+			mode = 2;
+		}
+		else if (temp == ".names") {
+			mode = 3;
+		}
+		else if(temp == ".end") break;
+		else
+			mode = 0;
+		cout << temp << endl;
+		
+		if (mode == -2) {
 			getline(blif_file, temp);
 			continue;
 		}
-		else if (temp == ".model")	{			//get module name
+		else if (mode == -1) {
 			if (blif_file.peek() != '\n')
 				blif_file >> module_name;
 			else
 				module_name = argv[1];
 		}
-		else if (temp == ".inputs") {
-			getline(blif_file, temp_line);
-			while (1) {
-			}
+		else if (mode == 0) {
+			continue;
 		}
-		else if (temp == ".outputs") {
-			// do {
-			// 	blif_file >> temp;
-			// 	if (temp == "\\") continue;
-			// 	else output.push_back(temp);
-			// } while ((temp == "\\" && (blif_file.peek() == '\n')) || ((temp != "\\") && (blif_file.peek() == '\n')));
+		else if (mode == 1) {
+			if (temp == "\\")
+				continue;
+			else
+				input.push_back(temp);
 		}
-		else if(temp == ".end") break;
-		cout << temp << endl;
+		else if (mode == 2) {
+			if (temp == "\\")
+				continue;
+			else
+				input.push_back(temp);
+		}
+		else if (mode == 3) {
+			cout << "f" << endl;
+		}
+		
 	}
 	blif_file.close();
 
