4aa85643dafe41442a0971ee1380a71c8f143a98
AnhPhan <anhphan21@outlook.com>
2023-10-04 14:23:54 +0800

    v0.1.1: read input, output complete; next: read fucntions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "./include/Graph.h"
 using namespace std;
 
-vector<string> input, output;
+
 
 int main(int argc, char* argv[]) {
 
@@ -13,79 +13,69 @@ int main(int argc, char* argv[]) {
 	int INV_MAX;
 
 	string module_name;
+	vector<string> in_list, out_list;
 
 	// Read blif file and extract data
 	ifstream 	blif_file(argv[1]);
 	int 		mode = 0;						//-2: read comment, -1: read module name, 0: read normal, 1: parse input, 2: parse output, 3: parse function
 	string 		temp;
 
+	
 	while(1) {
 		blif_file >> temp;
 		if (temp[0] == '#') {					//check if we get the comment part
-			mode = -2;
-		}
-		else if (temp == ".model")	{			//get module name
-			// if (blif_file.peek() != '\n')
-			// 	blif_file >> module_name;
-			// else
-			// 	module_name = argv[1];
-			mode = -1;
-		}
-		else if (temp == ".inputs") {
-			mode = 1;
-		}
-		else if (temp == ".outputs") {
-			mode = 2;
-		}
-		else if (temp == ".names") {
-			mode = 3;
-		}
-		else if(temp == ".end") break;
-		else
-			mode = 0;
-		cout << temp << endl;
-		
-		if (mode == -2) {
 			getline(blif_file, temp);
 			continue;
 		}
-		else if (mode == -1) {
+		else if (temp == ".model")	{			//get module name
 			if (blif_file.peek() != '\n')
 				blif_file >> module_name;
 			else
 				module_name = argv[1];
 		}
-		else if (mode == 0) {
+		else if (temp == ".inputs") {
+			mode = 1;
 			continue;
 		}
-		else if (mode == 1) {
+		else if (temp == ".outputs") {
+			mode = 2;
+			continue;
+		}
+		else if (temp == ".names") {
+			mode = 3;
+			continue;
+		}
+		else if(temp == ".end") break;
+
+		if (mode == 1) {
 			if (temp == "\\")
 				continue;
 			else
-				input.push_back(temp);
+				in_list.push_back(temp);
 		}
 		else if (mode == 2) {
 			if (temp == "\\")
 				continue;
 			else
-				input.push_back(temp);
+				out_list.push_back(temp);
 		}
 		else if (mode == 3) {
-			cout << "f" << endl;
+			
 		}
-		
 	}
+
 	blif_file.close();
+	cout << "Done read file" << endl;
 
 	// Print file result
 	cout << module_name << endl;
 	cout << "Inputs: " ;
-	for (int i = 0; i < input.size()-1; i++)
-		cout << input[i] << " ";
+	for (int i = 0; i < in_list.size(); i++)
+		cout << in_list[i] << " ";
 	cout << endl;
 	cout << "Outputs: ";
-	for (int i = 0; i < output.size()-1; i++)
-		cout << input[i] << " ";
+	for (int i = 0; i < out_list.size(); i++)
+		cout << out_list[i] << " ";
 	cout << endl;
 	cout << "-----------------------------" << endl;
 	return 0;
